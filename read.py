81d4f21b860f11bfd715e4ac9275d23306259455
VAX <oralpuyo@gmail.com>
2022-02-19 21:36:28 +0800

    calc

--- read.py
+++ read.py
@@ -4,11 +4,12 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if count % 1000 == 0:
+		if count % 100000 == 0:
 			print(len(data))
-	                 
-print(len(data))
+print('檔案讀取完畢,總共有', len(data), '筆資料')
 
-print(data[0])
-print('-------------------')
-print(data[1])
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+
+print('平均每筆留言長度是', sum_len/len(data))
